e8baba72a83de91dc713279680e2c17efb1197f9
Santosh Gorale <santoshgorale@gmail.com>
2019-02-06 23:40:50 +0530

    Setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = env => {
     plugins: [CSSExtract],
     devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
-      contentBase: path.join(__dirname, "/public"),
+      contentBase: path.join(__dirname, "public"),
       historyApiFallback: true
     }
   };
